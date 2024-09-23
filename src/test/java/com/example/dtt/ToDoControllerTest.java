56f11640f43723d750b6b9c8d474462bcda8c705
Mostafa Mosad <mosmostafa.c@stc.com.sa>
2024-09-23 19:31:53 +0300

    Change SpringExtension to MockitoExtension.

--- src/test/java/com/example/dtt/ToDoControllerTest.java
+++ src/test/java/com/example/dtt/ToDoControllerTest.java
@@ -3,6 +3,7 @@ package com.example.dtt;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 @WebMvcTest
 public class ToDoControllerTest {
 
